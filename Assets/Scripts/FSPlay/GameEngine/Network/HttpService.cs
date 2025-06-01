452eafa9462ddcdc3bb13f32cf9174acdb903ad4
Ngô Trí Nhật <nhatactvn@gmail.com>
2025-06-01 11:41:00 +0700

    Update code HttpService.cs

--- Assets/Scripts/FSPlay/GameEngine/Network/HttpService.cs
+++ Assets/Scripts/FSPlay/GameEngine/Network/HttpService.cs
@@ -2,11 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Collections;
+using UnityEngine.Networking;
 
 /// <summary>
-/// http方式的资源加载。 该类是异步加载
+/// HTTP resource loading service (asynchronous loading).
 /// </summary>
-
 public class HttpService : TTMonoBehaviour
 {
     static HttpService sInstance = null;
@@ -33,63 +33,52 @@ public class HttpService : TTMonoBehaviour
         }
     }
 
-    //Load callback
-    public Action<WWW, object> LoadFinishDelegate = null;
-    public Action<WWW> LoadProgressDelegate = null;
+    // Callback delegates
+    public Action<UnityWebRequest, object> LoadFinishDelegate = null;
+    public Action<UnityWebRequest> LoadProgressDelegate = null;
 
     public class URLRequest
     {
         public const int DEFAULT_TIMEOUT = 30;
         public string Url;
-        public Action<WWW, object> callbacks;
-        public Action<WWW> progressCallbacks;
-        public WWW wwwObject = null;
+        public Action<UnityWebRequest, object> callbacks;
+        public Action<UnityWebRequest> progressCallbacks;
+        public UnityWebRequest webRequest = null;
         public WWWForm form = null;
-        public List<object> callbackParmas = new List<object>();
+        public List<object> callbackParams = new List<object>();
         public float startTime = 0f;
         public int Timeout = 0;
-  
 
-        public URLRequest(string url, Action<WWW, object> callback, Action<WWW> progressCallback, object param = null, WWWForm form = null,byte[] postData = null, int timeout = 0)
+        public URLRequest(string url, Action<UnityWebRequest, object> callback, Action<UnityWebRequest> progressCallback, object param = null, WWWForm form = null, byte[] postData = null, int timeout = 0)
         {
             Url = url;
             callbacks = callback;
             progressCallbacks = progressCallback;
             startTime = Time.realtimeSinceStartup;
-            if (timeout <= 0)
-            {
-                timeout = DEFAULT_TIMEOUT;
-            }
-            Timeout = timeout;
+            Timeout = timeout <= 0 ? DEFAULT_TIMEOUT : timeout;
+
             if (postData != null)
             {
-                //Hashtable hashTable = new Hashtable();
-                //hashTable.Add("Content-Type", "application/octet-stream");
-                //wwwObject = new WWW(url, postData, hashTable);
-                Dictionary<string, string> dict = new Dictionary<string, string>();
-                dict.Add("Content-Type", "application/octet-stream");
-                wwwObject = new WWW(url, postData, dict);
+                webRequest = UnityWebRequest.Post(url, form);
+                webRequest.uploadHandler = new UploadHandlerRaw(postData);
+                webRequest.downloadHandler = new DownloadHandlerBuffer();
             }
             else if (form != null)
             {
-                wwwObject = new WWW(url, form);
+                webRequest = UnityWebRequest.Post(url, form);
             }
             else
             {
-                wwwObject = new WWW(url);
+                webRequest = UnityWebRequest.Get(url);
             }
-            callbackParmas.Add(param);
+
+            callbackParams.Add(param);
         }
     }
 
     private Dictionary<string, URLRequest> DownloadList = new Dictionary<string, URLRequest>();
 
-    void Start()
-    {
-
-    }
-
-    public void Load(string url, Action<WWW, object> callback, object param, int timeout = 0)
+    public void Load(string url, Action<UnityWebRequest, object> callback, object param, int timeout = 0)
     {
         if (String.IsNullOrEmpty(url))
         {
@@ -97,173 +86,100 @@ public class HttpService : TTMonoBehaviour
             return;
         }
         if (callback == null)
-            callback = (WWW www, object arg) => { };
-        if (timeout == null)
-            timeout = DEFAULT_TIMEOUT;
-        StartCoroutine<bool>(loadFromRemote(url, callback, (WWW arg) => { }, param, null,null, timeout = 0));
-    }
-
-    public void Load(string url, Action<WWW, object> callback, byte[] postData, object param, int timeout = 0)
-    {
-        if (String.IsNullOrEmpty(url))
-        {
-            KTDebug.LogError("[Http Service] URL is NULL");
-            return;
-        }
-        if (callback == null)
-            callback = (WWW www, object arg) => { };
-        if (timeout == null)
-            timeout = DEFAULT_TIMEOUT;
-        StartCoroutine<bool>(loadFromRemote(url, callback, (WWW arg) => { }, param, null, postData, timeout = 0));
-    }
-
-    /// <summary>
-    /// 发起HTTP请求
-    /// </summary>
-    /// <param name='url'>
-    ///  必选参数
-    /// </param>
-    /// <param name='callback'>
-    ///  完成后的回调,不用时可写null
-    /// </param>
-    /// <param name='timeout'>
-    /// 超时值,单位秒; 默认值 0 (代表30s默认)  超时后callback接收到的 WWW==null  以此判断是否超时
-    /// </param>
-    public void Load(string url, Action<WWW, object> callback, int timeout)
-    {
-        if (String.IsNullOrEmpty(url))
-        {
-            KTDebug.LogError("[Http Service] URL is NULL");
-            return;
-        }
-        if (callback == null)
-            callback = (WWW www, object arg) => { };
-        if (timeout == null)
-            timeout = DEFAULT_TIMEOUT;
-        StartCoroutine<bool>(loadFromRemote(url, callback, (WWW arg) => { }, null, null,null, timeout));
-    }
-    /// <summary>
-    /// 发起HTTP请求
-    /// </summary>
-    /// <param name='url'>
-    /// 必选参数
-    /// </param>
-    /// <param name='callback'>
-    /// 完成后的回调,不用时可写null
-    /// </param>
-    /// <param name='form'>
-    /// 表单信息,可为null
-    /// </param>
-    /// <param name='param'>
-    /// 希望完成后传入callback的参数,默认可为null
-    /// </param>
-    /// <param name='timeout'>
-    /// 超时值,单位秒; 默认值 0 (代表30s默认)  超时后callback接收到的 WWW==null  以此判断是否超时
-    /// </param>
-    public void Load(string url, Action<WWW, object> callback, WWWForm form, object param, int timeout)
-    {
-        if (String.IsNullOrEmpty(url))
-        {
-            KTDebug.LogError("[Http Service] URL is NULL");
-            return;
-        }
-        if (callback == null)
-            callback = (WWW www, object arg) => { };
-        if (timeout == null)
-            timeout = DEFAULT_TIMEOUT;
-        StartCoroutine<bool>(loadFromRemote(url, callback, (WWW arg) => { }, param, form,null, timeout));
-    }
-    /// <summary>
-    /// 发起HTTP请求
-    /// </summary>
-    /// <param name='url'>
-    /// 必选参数
-    /// </param>
-    /// <param name='callback'>
-    /// 完成后的回调,不用时可写null  回调格式(WWW,object)=>{}
-    /// </param>
-    /// <param name='progressCallback'>
-    /// 进度回调,不用时可写null 回调格式(WWW)=>{} 
-    /// </param>
-    /// <param name='param'>
-    /// 希望完成后传入callback的参数,默认可为null
-    /// </param>
-    /// <param name='timeout'>
-    /// 超时值,单位秒; 默认值 0 (代表30s默认)  超时后callback接收到的 WWW==null  以此判断是否超时
-    /// </param>
-    public void Load(string url, Action<WWW, object> callback, Action<WWW> progressCallback, object param, int timeout = 0)
-    {
-        if (String.IsNullOrEmpty(url))
-        {
-            KTDebug.LogError("[Http Service] URL is NULL");
-            return;
-        }
-        if (callback == null)
-            callback = (WWW www, object arg) => { };
-        if (progressCallback == null)
-            progressCallback = (WWW arg) => { };
+            callback = (UnityWebRequest request, object arg) => { };
         if (timeout == 0)
             timeout = DEFAULT_TIMEOUT;
-        StartCoroutine<bool>(loadFromRemote(url, callback, progressCallback, param, null,null, timeout));
+
+        StartCoroutine(loadFromRemote(url, callback, (UnityWebRequest req) => { }, param, null, null, timeout));
     }
 
-    IEnumerator loadFromRemote(string url, Action<WWW, object> callback, Action<WWW> progressCallback, object param = null, WWWForm form = null, byte[] postData = null,int timeout = 0)
+    public void Load(string url, Action<UnityWebRequest, object> callback, byte[] postData, object param, int timeout = 0)
     {
-        yield return 1;
+        if (String.IsNullOrEmpty(url))
+        {
+            KTDebug.LogError("[Http Service] URL is NULL");
+            return;
+        }
+        if (callback == null)
+            callback = (UnityWebRequest request, object arg) => { };
+        if (timeout == 0)
+            timeout = DEFAULT_TIMEOUT;
+
+        StartCoroutine(loadFromRemote(url, callback, (UnityWebRequest req) => { }, param, null, postData, timeout));
+    }
+
+    IEnumerator loadFromRemote(string url, Action<UnityWebRequest, object> callback, Action<UnityWebRequest> progressCallback, object param = null, WWWForm form = null, byte[] postData = null, int timeout = 0)
+    {
+        UnityWebRequest request = new UnityWebRequest(url)
+        {
+            timeout = timeout
+        };
+
         if (DownloadList.ContainsKey(url))
         {
+            // Thêm callback vào danh sách chờ
             DownloadList[url].callbacks += callback;
             DownloadList[url].progressCallbacks += progressCallback;
-            DownloadList[url].callbackParmas.Add(param);
+            DownloadList[url].callbackParams.Add(param);
         }
         else
         {
             DownloadList.Add(url, new URLRequest(url, callback, progressCallback, param, form, postData, timeout));
         }
+
+        // Gửi yêu cầu và đợi phản hồi
+        yield return request.SendWebRequest();
+
+        // Kiểm tra lỗi kết nối và lỗi HTTP
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError($"Request failed: {request.error}");
+        }
+        else
+        {
+            // Khi tải thành công, gọi callback với dữ liệu trả về
+            callback?.Invoke(request, param);
+        }
+
+        // Cập nhật tiến độ nếu có
+        if (progressCallback != null)
+        {
+            progressCallback.Invoke(request);
+        }
     }
+
+
     int oldTime = 0;
     void Update()
     {
-        /*if(((int)Time.realtimeSinceStartup - oldTime)< 1 )
-        {
-            return;
-        }
-        oldTime  = (int)Time.realtimeSinceStartup;
-		
-        KTDebug.Log("Time"+Time.realtimeSinceStartup);*/
         List<string> completedItems = null;
 
-        foreach (KeyValuePair<string, URLRequest> wwwPair in DownloadList)
+        foreach (KeyValuePair<string, URLRequest> requestPair in DownloadList)
         {
-            URLRequest wwwReq = wwwPair.Value;
+            URLRequest request = requestPair.Value;
+            UnityWebRequest webRequest = request.webRequest;
 
-            if (wwwReq.wwwObject.isDone)
+            if (webRequest.isDone)
             {
-                //下载完成
-                Action<WWW, object> callbacks = (Action<WWW, object>)wwwReq.callbacks;
-                while (callbacks != null && callbacks.GetInvocationList().GetLength(0) > 0)
+                // Request completed, call the callback
+                Action<UnityWebRequest, object> callbacks = request.callbacks;
+                while (callbacks != null && callbacks.GetInvocationList().Length > 0)
                 {
-                    Action<WWW, object> callback = (Action<WWW, object>)callbacks.GetInvocationList()[0];
-                    if (!string.IsNullOrEmpty(wwwReq.wwwObject.error ))
-                    {
-                        KTDebug.LogWarning(this.name + " : " + wwwReq.wwwObject.error + " , " + wwwReq.Url);
-                    }
+                    Action<UnityWebRequest, object> callback = (Action<UnityWebRequest, object>)callbacks.GetInvocationList()[0];
                     try
                     {
                         callbacks -= callback;
-                        //销毁进度监听
-                        //(Action<WWW>)wwwReq.progressCallbacks -= (Action<WWW>)wwwReq.progressCallbacks.GetInvocationList()[0];
-                        wwwReq.progressCallbacks -= (Action<WWW>)wwwReq.progressCallbacks.GetInvocationList()[0];
-                        callback.Invoke(wwwReq.wwwObject, wwwReq.callbackParmas[0]);
+                        request.progressCallbacks -= (Action<UnityWebRequest>)request.progressCallbacks.GetInvocationList()[0];
+                        callback.Invoke(webRequest, request.callbackParams[0]);
                     }
                     catch (Exception e)
                     {
                         KTDebug.LogException(e);
                     }
 
-                    if (wwwReq.callbackParmas.Count > 0)
+                    if (request.callbackParams.Count > 0)
                     {
-                        wwwReq.callbackParmas.RemoveAt(0);
+                        request.callbackParams.RemoveAt(0);
                     }
                 }
 
@@ -271,45 +187,39 @@ public class HttpService : TTMonoBehaviour
                 {
                     completedItems = new List<string>();
                 }
-                completedItems.Add(wwwPair.Key);
+                completedItems.Add(requestPair.Key);
             }
             else
             {
-                //下载过程中  
-                if (wwwReq.progressCallbacks != null)
+                // Request still in progress, handle progress update
+                if (request.progressCallbacks != null)
                 {
                     try
                     {
-                        if (Time.realtimeSinceStartup - wwwReq.startTime > wwwReq.Timeout)
+                        // Timeout check
+                        if (Time.realtimeSinceStartup - request.startTime > request.Timeout)
                         {
-                            //超时处理
-                            KTDebug.LogWarning("[Http Service]" + wwwReq.Url + ", TIMEOUT");
+                            KTDebug.LogWarning("[Http Service] Timeout for " + request.Url);
 
-                            //下载完成
-                            Action<WWW, object> callbacks = (Action<WWW, object>)wwwReq.callbacks;
-                            while (callbacks != null && callbacks.GetInvocationList().GetLength(0) > 0)
+                            // Handle timeout
+                            Action<UnityWebRequest, object> callbacks = request.callbacks;
+                            while (callbacks != null && callbacks.GetInvocationList().Length > 0)
                             {
-                                Action<WWW, object> callback = (Action<WWW, object>)callbacks.GetInvocationList()[0];
-                                if (!string.IsNullOrEmpty(wwwReq.wwwObject.error))
-                                {
-                                    KTDebug.LogWarning(wwwReq.wwwObject.error);
-                                }
+                                Action<UnityWebRequest, object> callback = (Action<UnityWebRequest, object>)callbacks.GetInvocationList()[0];
                                 try
                                 {
                                     callbacks -= callback;
-                                    //销毁进度监听
-                                    //(Action<WWW>)wwwReq.progressCallbacks -= (Action<WWW>)wwwReq.progressCallbacks.GetInvocationList()[0];
-                                    wwwReq.progressCallbacks -= (Action<WWW>)wwwReq.progressCallbacks.GetInvocationList()[0];
-                                    callback.Invoke(null, wwwReq.Url);
+                                    request.progressCallbacks -= (Action<UnityWebRequest>)request.progressCallbacks.GetInvocationList()[0];
+                                    callback.Invoke(null, request.Url);
                                 }
                                 catch (Exception e)
                                 {
                                     KTDebug.LogException(e);
                                 }
 
-                                if (wwwReq.callbackParmas.Count > 0)
+                                if (request.callbackParams.Count > 0)
                                 {
-                                    wwwReq.callbackParmas.RemoveAt(0);
+                                    request.callbackParams.RemoveAt(0);
                                 }
                             }
 
@@ -317,15 +227,13 @@ public class HttpService : TTMonoBehaviour
                             {
                                 completedItems = new List<string>();
                             }
-                            completedItems.Add(wwwPair.Key);
+                            completedItems.Add(requestPair.Key);
                         }
                         else
                         {
-                            //更新进度
-                            //KTDebug.LogWarning("^^^"+wwwReq.Url+","+wwwReq.Timeout+","+(Time.realtimeSinceStartup-wwwReq.startTime).ToString());
-                            ((Action<WWW>)wwwReq.progressCallbacks.GetInvocationList()[0]).Invoke(wwwReq.wwwObject);
+                            // Update progress
+                            ((Action<UnityWebRequest>)request.progressCallbacks.GetInvocationList()[0]).Invoke(webRequest);
                         }
-
                     }
                     catch (Exception e)
                     {
@@ -333,19 +241,15 @@ public class HttpService : TTMonoBehaviour
                     }
                 }
             }
-
         }
 
         if (completedItems != null)
         {
-            for (int i = 0; i < completedItems.Count; i++)
+            foreach (string key in completedItems)
             {
-                DownloadList.Remove(completedItems[i]);
+                DownloadList.Remove(key);
             }
             completedItems.Clear();
-            completedItems = null;
         }
-
     }
-
 }
